fb0b5b1444bbd8733b6e325559cb7a00454c7525
Varun Naik <naik.varun@gmail.com>
2020-12-26 21:12:27 -0600

    Added cases for palindrome.

--- src/test/strings/palindrome-check.spec.ts
+++ src/test/strings/palindrome-check.spec.ts
@@ -4,8 +4,44 @@ describe('Is Palindrome', () => {
     test('#1', () => {
         expect(isPalindrome('abcdcba')).toBeTruthy();
     });
-
     test('#2', () => {
-        expect(isPalindrome('abcd')).toBeFalsy();
+        expect(isPalindrome('a')).toBeTruthy();
+    });
+    test('#3', () => {
+        expect(isPalindrome('ab')).toBeFalsy();
+    });
+    test('#4', () => {
+        expect(isPalindrome('aba')).toBeTruthy();
+    });
+    test('#5', () => {
+        expect(isPalindrome('abb')).toBeFalsy();
+    });
+    test('#6', () => {
+        expect(isPalindrome('abba')).toBeTruthy();
+    });
+    test('#7', () => {
+        expect(isPalindrome('abcdefghhgfedcba')).toBeTruthy();
+    });
+    test('#8', () => {
+        expect(isPalindrome('abcdefghihgfedcba')).toBeTruthy();
+    });
+    test('#9', () => {
+        expect(isPalindrome('abcdefghihgfeddcba')).toBeFalsy();
     });
 });
+
+/*const cases: {string: string, output : boolean }[] = [{string: "abcdcba", output: true},
+    {string: "a", output: true},
+    {string: "ab", output: false},
+    {string: "aba", output: true},
+    {string: "abb", output: false},
+    {string: "abba",  output: true},
+    {string: "abcdefghhgfedcba", output: true},
+    {string: "abcdefghihgfedcba", output: true},
+    {string: "abcdefghihgfeddcba", output: false}]
+
+cases.forEach((item, index) => {
+    console.log(`test('#${index+1}', () => {
+        expect(isPalindrome('${item.string}')).toBe${item.output ? 'Truthy': 'Falsy'}();
+    });`)
+});*/
