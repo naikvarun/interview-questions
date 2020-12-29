75acdd27174d4c80c9ec676f758e2f1ed0763b71
Varun Naik <naik.varun@gmail.com>
2020-12-29 15:58:39 -0600

    Added BST class

---
+++ src/app/bst/bst.ts
@@ -0,0 +1,11 @@
+export class BST {
+    value: number;
+    left: BST | null;
+    right: BST | null;
+
+    constructor(value: number) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
